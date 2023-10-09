4edea43b1e03f68c5c9f7b944788a07a9daeb7f4
Anastasia Adamoudi <53370412+AnastasiaAdamoudi@users.noreply.github.com>
2023-10-09 22:34:27 +0300

    Added functionality to the submit button - missing function to check if shifts have enough employees

--- employee-roster/src/components/form/form.jsx
+++ employee-roster/src/components/form/form.jsx
@@ -4,6 +4,7 @@ import ShiftDetails from "../shift-details/shift-details";
 import EmployeeDetails from "../employee-details/employee-details";
 
 function MyForm() {
+
   const [bankShiftsExistence, setBankShiftsExistence] = useState(false);
   const [shiftData, setShiftData] = useState(
     Array.from({ length: 7 }, () => ({
@@ -37,8 +38,15 @@ function MyForm() {
     setBankShiftsExistence(value);
   };
 
-  const dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
+  const dayNames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
   const handleShiftDataChange = (index, field, value) => {
     const updatedShiftData = [...shiftData];
@@ -81,10 +89,81 @@ function MyForm() {
     setEmployeeData(updatedEmployeeData);
   };
 
+  // Function to handle form submission
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(employeeData);
+    const updatedShiftData = calculateShiftDurations(shiftData); // Step 1: Calculate the duration of each main shift and bank shift
+    const employeeAssignments = distributeEmployeesToMainShifts(updatedShiftData, employeeData); // Step 2: Distribute employees to main shifts
+    const updatedEmployeeData = offerBankShiftsIfNeeded(employeeAssignments, employeeData); // Step 3: Offer additional bank shifts to employees if needed
+    const shiftAvailability = checkShiftAvailability(updatedShiftData, updatedEmployeeData); // Step 4: Check if there are enough employees for each shift
+    if (shiftAvailability.some((availability) => !availability)) {
+      alert("There are not enough employees for some shifts!"); // Step 5: Display an alert if there are any issues
+    } else {
+      console.log("Shift data:", updatedShiftData);
+      console.log("Employee data:", updatedEmployeeData);
+    }
   };
+  
+  // Function to calculate the duration of each shift
+  const calculateShiftDurations = (shiftData) => {
+    const updatedShiftData = [...shiftData];
+    for (let i = 0; i < updatedShiftData.length; i++) {
+      const shift = updatedShiftData[i];
+      shift.shifts.forEach((s) => {
+        const startTime = new Date(`1970-01-01T${s.StartTime}`);
+        const endTime = new Date(`1970-01-01T${s.EndTime}`);
+        const duration = (endTime - startTime) / (60 * 1000);
+        s.duration = duration;
+      });
+    }
+    return updatedShiftData;
+  };
+  
+  // Function to distribute employees to main shifts
+  const distributeEmployeesToMainShifts = (shiftData, employeeData) => {
+    const employeeAssignments = [];
+    for (let i = 0; i < shiftData.length; i++) {
+      const shift = shiftData[i];
+      const mainShifts = shift.shifts;
+      const employees = employeeData.filter((e) => e.days[dayNames[i].toLowerCase()]);
+      const employeeAssignmentsForDay = [];
+      for (let j = 0; j < mainShifts.length; j++) {
+        const shift = mainShifts[j];
+        const employeesForShift = employees.filter((e) => e.totalHours >= shift.duration);
+        employeeAssignmentsForDay.push(employeesForShift);
+      }
+      employeeAssignments.push(employeeAssignmentsForDay);
+    }
+    return employeeAssignments;
+  };
+  
+  // Function to offer additional bank shifts to employees if needed
+  const offerBankShiftsIfNeeded = (employeeAssignments, employeeData) => {
+    const updatedEmployeeData = [...employeeData];
+    for (let i = 0; i < employeeAssignments.length; i++) {
+      const assignmentsForDay = employeeAssignments[i];
+      const employeesForDay = updatedEmployeeData.filter((e) => e.days[dayNames[i].toLowerCase()]);
+      for (let j = 0; j < assignmentsForDay.length; j++) {
+        const assignmentsForShift = assignmentsForDay[j];
+        const employeesForShift = employeesForDay.filter((e) => assignmentsForShift.includes(e));
+        const totalHoursForShift = assignmentsForShift.reduce((total, e) => total + e.totalHours, 0);
+        const totalHoursForEmployees = employeesForShift.reduce((total, e) => total + e.totalHours, 0);
+        if (totalHoursForShift > totalHoursForEmployees) {
+          const bankShifts = totalHoursForShift - totalHoursForEmployees;
+          const bankShiftsPerEmployee = Math.ceil(bankShifts / employeesForShift.length);
+          employeesForShift.forEach((e) => {
+            e.totalHours += bankShiftsPerEmployee;
+          });
+        }
+      }
+    }
+    return employeeAssignments;
+  };
+  
+  // Function to check if there are enough employees for each main shift and bank shift
+  const checkShiftAvailability = (shiftData, employeeDatal, employeeAssignments) => {
+
+      
 
   return (
     <div>
@@ -92,7 +171,9 @@ function MyForm() {
         {Array.from({ length: 7 }).map((_, index) => (
           <div key={index}>
             <Form.Group controlId={`formBasicMainShifts${index}`}>
-            <Form.Label>Number of main shifts for {dayNames[index]}</Form.Label>
+              <Form.Label>
+                Number of main shifts for {dayNames[index]}
+              </Form.Label>
               <Form.Control
                 type="number"
                 placeholder={`Enter the number of main shifts for day ${dayNames[index]}`}
@@ -199,14 +280,17 @@ function MyForm() {
                 {employeeData[index] && (
                   <EmployeeDetails
                     key={index}
-                    index={index}
-                    name={employeeData[index].name}
-                    totalHours={employeeData[index].totalHours}
-                    days={employeeData[index].days}
+                    {...employeeData[index]}
                     dayNames={dayNames}
-                    onEmployeeNameChange={handleEmployeeNameChange}
-                    onEmployeeTotalHoursChange={handleEmployeeTotalHoursChange}
-                    onEmployeeDaysChange={handleEmployeeDaysChange}
+                    onEmployeeNameChange={(value) =>
+                      handleEmployeeNameChange(index, value)
+                    }
+                    onEmployeeTotalHoursChange={(value) =>
+                      handleEmployeeTotalHoursChange(index, value)
+                    }
+                    onEmployeeDaysChange={(days) =>
+                      handleEmployeeDaysChange(index, days)
+                    }
                   />
                 )}
               </div>
